8f9cf46d1ceb86d4b4481ffa9f040200d1f6ced3
Tim Bulokhov <timofeybrant@mail.ru>
2024-09-10 17:55:19 +0200

    M1 [Profile] Вёрстка профиля. Имя пользователя

--- FakeNFT/Profile/ProfileViewController.swift
+++ FakeNFT/Profile/ProfileViewController.swift
@@ -10,14 +10,18 @@ import UIKit
 final class ProfileViewController: UIViewController {
     
     private lazy var profileAvatar = UIImageView(image: UIImage(named: "avatarPlug"))
+    private lazy var profileName = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(named: "YPWhite")
-        configProfileAvatar()
+        configProfileViewController()
     }
     
-    private func configProfileAvatar() {
+    private func configProfileViewController() {
+        
+        //Настройка отображения изображения профиля
+        
         profileAvatar.layer.masksToBounds = true
         profileAvatar.clipsToBounds = true
         profileAvatar.layer.cornerRadius = 35
@@ -30,6 +34,21 @@ final class ProfileViewController: UIViewController {
             profileAvatar.heightAnchor.constraint(equalToConstant: 70),
             profileAvatar.topAnchor.constraint(equalTo: view.topAnchor, constant: 108),
             profileAvatar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
+        ])
+        
+        //Настройка отображения имени профиля
+        
+        profileName.textColor = UIColor(named: "YPBlack")
+        profileName.font = UIFont.headline3
+        profileName.text = "Timofey Bulokhov"
+        
+        profileName.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(profileName)
+        
+        NSLayoutConstraint.activate([
+            profileName.centerYAnchor.constraint(equalTo: profileAvatar.centerYAnchor),
+            profileName.leadingAnchor.constraint(equalTo: profileAvatar.trailingAnchor, constant: 16),
+            profileName.trailingAnchor.constraint(greaterThanOrEqualTo: view.trailingAnchor, constant: -16)
         ])
     }
 }
