b18bb96ae9d8af39dde9a757435d9f0415137c3e
Tim Bulokhov <timofeybrant@mail.ru>
2024-09-10 18:05:12 +0200

    M1 [Profile] Вёрстка профиля. Описание пользователя

--- FakeNFT/Profile/ProfileViewController.swift
+++ FakeNFT/Profile/ProfileViewController.swift
@@ -11,6 +11,7 @@ final class ProfileViewController: UIViewController {
     
     private lazy var profileAvatar = UIImageView(image: UIImage(named: "avatarPlug"))
     private lazy var profileName = UILabel()
+    private lazy var profileDescription = UITextView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,5 +51,37 @@ final class ProfileViewController: UIViewController {
             profileName.leadingAnchor.constraint(equalTo: profileAvatar.trailingAnchor, constant: 16),
             profileName.trailingAnchor.constraint(greaterThanOrEqualTo: view.trailingAnchor, constant: -16)
         ])
-    }
+        
+        //Настройка отображения описания профиля
+        
+        profileDescription.backgroundColor = .clear
+        profileDescription.isEditable = false
+        profileDescription.isScrollEnabled = false
+        profileDescription.sizeToFit()
+        
+        profileDescription.textContainer.lineFragmentPadding = 0
+        profileDescription.textContainerInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        profileDescription.textAlignment = .left
+        profileDescription.textContainer.maximumNumberOfLines = 5
+            
+            let text = "Something looks like description ✅"
+            
+            let style = NSMutableParagraphStyle()
+            style.lineSpacing =  3
+            let attributes = [NSAttributedString.Key.paragraphStyle : style,
+                              .foregroundColor: UIColor(named: "YPBlack"),
+                              .font: UIFont.caption2
+            ]
+            
+        profileDescription.attributedText = NSAttributedString(string: text, attributes: attributes as [NSAttributedString.Key : Any])
+            
+        profileDescription.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(profileDescription)
+            
+            NSLayoutConstraint.activate([
+                profileDescription.topAnchor.constraint(equalTo: profileAvatar.bottomAnchor, constant: 20),
+                profileDescription.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+                profileDescription.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+            ])
+        }
 }
