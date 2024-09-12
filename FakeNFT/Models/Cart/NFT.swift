06abe54da673e40411ae03a54dd480f847434a51
Maria <manya0705@yandex.ru>
2024-09-12 03:55:28 +0300

    cart1-3. создана таблица для корзины, созданы ячейки корзины с наименованием/рейтингом/ценой и кнопокой удаления корзины

---
+++ FakeNFT/Models/Cart/NFT.swift
@@ -0,0 +1,15 @@
+//
+//  NftCartModel.swift
+//  FakeNFT
+//
+//  Created by Мария Шагина on 11.09.2024.
+//
+
+import UIKit
+
+struct NFT {
+    let image: UIImage
+    let name: String
+    let rating: UIImage
+    let price: String
+}
---
+++ FakeNFT/Scenes /Catalog/ProfileVC.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileVC.swift
+//  FakeNFT
+//
+//  Created by Мария Шагина on 10.09.2024.
+//
+import UIKit
+
+final class ProfileVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
