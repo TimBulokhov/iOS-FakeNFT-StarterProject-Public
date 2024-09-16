d9b0d5398190f4f30770566832c3df7cb5b65ac0
Maria <manya0705@yandex.ru>
2024-09-16 23:06:03 +0300

    Cart 1/3. Initial commit

---
+++ FakeNFT/Scenes /Catalog/StatisticVC.swift
@@ -0,0 +1,16 @@
+//
+//  StatisticVC.swift
+//  FakeNFT
+//
+//  Created by Мария Шагина on 16.09.2024.
+//
+
+import UIKit
+
+final class StatisticVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -1,25 +1,33 @@
 import UIKit
 
 final class TabBarController: UITabBarController {
-
+    
     var servicesAssembly: ServicesAssembly!
-
-    private let catalogTabBarItem = UITabBarItem(
-        title: NSLocalizedString("Tab.catalog", comment: ""),
-        image: UIImage(systemName: "square.stack.3d.up.fill"),
-        tag: 0
-    )
-
+    
+    private let catalogTabBarItem = UITabBarItem(title: NSLocalizedString("Tab.catalog", comment: ""), image: UIImage(systemName: "rectangle.stack.fill"), tag: 0)
+    private let profileTabBarItem = UITabBarItem(title: "Профиль", image: UIImage(systemName: "person.crop.circle.fill"), tag: 0)
+    private let cartTabBarItem = UITabBarItem(title: "Корзина", image: UIImage(named:"basketTabIcon"), tag: 0)
+    private let statisticTabBarItem = UITabBarItem(title: "Статистика", image: UIImage(systemName: "flag.2.crossed.fill"), tag: 0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let catalogController = TestCatalogViewController(
             servicesAssembly: servicesAssembly
         )
+        let profileController = ProfileVC()
+        let cartController = CartVC()
+        let statisticController = StatisticVC()
+        
+        self.tabBar.unselectedItemTintColor = .black
+        
+        profileController.tabBarItem = profileTabBarItem
         catalogController.tabBarItem = catalogTabBarItem
-
-        viewControllers = [catalogController]
-
+        cartController.tabBarItem = cartTabBarItem
+        statisticController.tabBarItem = statisticTabBarItem
+        
+        viewControllers = [profileController, catalogController, cartController, statisticController]
+        
         view.backgroundColor = .systemBackground
     }
 }
