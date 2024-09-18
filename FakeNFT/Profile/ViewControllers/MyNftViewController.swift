e9c1126ce75cc111cbacb39c3569e19f1b40280a
Tim Bulokhov <timofeybrant@mail.ru>
2024-09-18 21:09:03 +0200

    M2 [Profile] Сверстать заголовок на пустом экране

--- FakeNFT/Profile/ViewControllers/MyNftViewController.swift
+++ FakeNFT/Profile/ViewControllers/MyNftViewController.swift
@@ -11,18 +11,33 @@ final class MyNftViewController: UIViewController {
     
     private lazy var myNftTitleLabel = UILabel()
     private lazy var topViewsContainer = UIView()
+    private lazy var emptyNftLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(named: "YPWhite")
-        
+        configTopViewsContainer()
         configMyNftTitleLabel()
+        configEmptyNftLabel()
         
+    }
+    
+    private func configTopViewsContainer() {
+        topViewsContainer.backgroundColor = UIColor(named: "YPWhite")
+        topViewsContainer.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(topViewsContainer)
+        
+        NSLayoutConstraint.activate([
+            topViewsContainer.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            topViewsContainer.heightAnchor.constraint(equalToConstant: 42),
+            topViewsContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            topViewsContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+        ])
     }
     
     private func configMyNftTitleLabel(){
         myNftTitleLabel.textColor = UIColor(named: "YPBlack")
-        //myNftTitleLabel.isHidden = true
+        myNftTitleLabel.isHidden = true
         myNftTitleLabel.text = "Мои NFT"
         myNftTitleLabel.font = UIFont.bodyBold
         
@@ -32,6 +47,24 @@ final class MyNftViewController: UIViewController {
         NSLayoutConstraint.activate([
             myNftTitleLabel.centerYAnchor.constraint(equalTo: topViewsContainer.centerYAnchor),
             myNftTitleLabel.centerXAnchor.constraint(equalTo: topViewsContainer.centerXAnchor)
+        ])
+    }
+    
+    private func configEmptyNftLabel(){
+        emptyNftLabel.textColor = UIColor(named: "YPBlack")
+        emptyNftLabel.isHidden = true
+        
+        emptyNftLabel.text = "У вас ещё нет NFT"
+        emptyNftLabel.textAlignment = .center
+        emptyNftLabel.font = .bodyBold
+        emptyNftLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(emptyNftLabel)
+        
+        NSLayoutConstraint.activate([
+            emptyNftLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            emptyNftLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 16),
+            emptyNftLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            emptyNftLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 30)
         ])
     }
 
