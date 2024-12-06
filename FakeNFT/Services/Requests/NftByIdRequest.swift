876bad733c0f263c6615a64fd92865c37b026741
Tim Bulokhov <timofeybrant@mail.ru>
2024-12-06 15:02:37 +0200

    Fix

---
+++ FakeNFT/Services/Requests/NftByIdRequest.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct NFTRequest: NetworkRequest {
+
+    let id: String
+
+    var endpoint: URL? {
+        URL(string: "\(RequestConstants.baseURL)/api/v1/nft/\(id)")
+    }
+}
+
